c77fd70be86b16a1621b965494acbe71bff00c95
Takaaki-MATSUZAWA <matsuzawa@furo.org>
2024-09-06 22:58:09 +0900

    アイコンとライセンスの追加

--- src/main.rs
+++ src/main.rs
@@ -11,7 +11,13 @@ use egui_monitor::VisuModiRam;
 fn main() -> Result<(), eframe::Error> {
     env_logger::init(); // Log to stderr (if you run with `RUST_LOG=debug`).
     let options = eframe::NativeOptions {
-        viewport: egui::ViewportBuilder::default().with_inner_size([1280.0, 720.0]),
+        viewport: egui::ViewportBuilder::default()
+            .with_inner_size([1280.0, 720.0])
+            .with_drag_and_drop(true)
+            .with_icon(
+                eframe::icon_data::from_png_bytes(&include_bytes!("../assets/icon_256.png")[..])
+                    .expect("Failed to load icon"),
+            ),
         ..Default::default()
     };
     eframe::run_native(
